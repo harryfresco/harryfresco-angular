8715fc2e98a628df59a3157dafcda93a3a385a1d
Harry Fresco <harryfresco99@gmail.com>
2024-11-21 16:37:35 +0000

    build

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/harryfresco-angular/',
+  basePath: '/harryfresco-angular',
   entryPoints: new Map([['', () => import('./main.server.mjs')]]),
 };
   
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -5,14 +5,69 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "redirectTo": "/harryfresco-angular/landing",
     "route": "/harryfresco-angular"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/analogue/june22"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/analogue/jan23"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/analogue/may23"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/analogue/aug23"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/analogue/june24"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/landing"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/choose"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/digital/wizard"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/digital/june24d"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/digital/japan"
+  },
+  {
+    "renderMode": 2,
+    "route": "/harryfresco-angular/digital/sky"
   }
 ],
   assets: new Map([
-['index.csr.html', {size: 5375, hash: 'cb06280d155738f716f12833db0c071d9f4b4142416cc79172384091282ac2d5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1125, hash: 'ae6954ee9a4af029d5ec5d7948fa54ffa4995943ec454f4c79b30fe6374f7e2c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
-['index.html', {size: 20651, hash: '547052a8c561c8b191e08d17a1e21880b7f0af78e8786f27161f49f51b523687', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)}], 
-['styles-YSZDR4PV.css', {size: 312537, hash: 'rTaopxAJbGk', text: () => import('./assets-chunks/styles-YSZDR4PV_css.mjs').then(m => m.default)}]
+['index.csr.html', {size: 5403, hash: '6e1133a34d4a2b85c86b1c9fe8df434aa0dcecb0052a768bd716b884d3eddb82', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1123, hash: 'fb6e41fcb7113f83d65735e91f88c774adefa015cce770c8a5f03d6ca2d6909c', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['r/analogue/june22/index.html', {size: 24499, hash: '6e7a15693ce820590469c78e7a614df2611cad122a908de75a010d30cd38e7fd', text: () => import('./assets-chunks/r_analogue_june22_index_html.mjs').then(m => m.default)}], 
+['r/analogue/jan23/index.html', {size: 24554, hash: '3cfede50d16965aa1d7ba131a20237cc2f78dc39af8b87bff27715ea63a7ac03', text: () => import('./assets-chunks/r_analogue_jan23_index_html.mjs').then(m => m.default)}], 
+['r/analogue/may23/index.html', {size: 24279, hash: '6e9c1ac2cdf263bee91a3428de4d325b6ef58a284e720925cd5e5a6f41e6d280', text: () => import('./assets-chunks/r_analogue_may23_index_html.mjs').then(m => m.default)}], 
+['r/landing/index.html', {size: 10330, hash: '2f9ba5bb55b152f2dfda15106dc028941e932ea4f09c1d202decdbfb84bfed7a', text: () => import('./assets-chunks/r_landing_index_html.mjs').then(m => m.default)}], 
+['r/analogue/june24/index.html', {size: 24950, hash: '628fa655c66092265a8de659f8d68bcbb640764cabd60a6fee68293bdac20c98', text: () => import('./assets-chunks/r_analogue_june24_index_html.mjs').then(m => m.default)}], 
+['r/analogue/aug23/index.html', {size: 24323, hash: '89f1b7f9aa82cbca7f90f6e859da423b5cf71ca3de8c2fb6e48ad7636992d01f', text: () => import('./assets-chunks/r_analogue_aug23_index_html.mjs').then(m => m.default)}], 
+['r/digital/wizard/index.html', {size: 22953, hash: 'ef307e8cf51cce6bd7b182f790c8bbf45c847d264f766fe6afd22be31c5f7620', text: () => import('./assets-chunks/r_digital_wizard_index_html.mjs').then(m => m.default)}], 
+['r/choose/index.html', {size: 11563, hash: 'd489edcb154e0226bf3f5927fdd90309e610cb7154cdba61ab2360df8d18682e', text: () => import('./assets-chunks/r_choose_index_html.mjs').then(m => m.default)}], 
+['r/digital/june24d/index.html', {size: 23289, hash: 'b19c970f37054f7d66b42cc516dd0fe6be4ef9a0ec4e1955125a97032a68a432', text: () => import('./assets-chunks/r_digital_june24d_index_html.mjs').then(m => m.default)}], 
+['r/digital/sky/index.html', {size: 22937, hash: '3081ee9fff4b85d6ba222b41de64efdce6ef7ba3c17994c393b09f689c2a606c', text: () => import('./assets-chunks/r_digital_sky_index_html.mjs').then(m => m.default)}], 
+['r/digital/japan/index.html', {size: 25053, hash: '87eeff49a9d68bb1b4d9e48488b8d52731faea54ce28071fbb3f2f7ba5d6f723', text: () => import('./assets-chunks/r_digital_japan_index_html.mjs').then(m => m.default)}], 
+['styles-6VKC4MKN.css', {size: 324815, hash: 'khyWumHA4mU', text: () => import('./assets-chunks/styles-6VKC4MKN_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
 };
