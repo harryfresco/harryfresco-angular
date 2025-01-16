4ed48703ff3d6b925ce14b80e6957b56d0de97a5
Harry Fresco <harryfresco99@gmail.com>
2025-01-16 10:17:59 +0000

    added Desktop Safari to test

--- playwright.config.ts
+++ playwright.config.ts
@@ -44,10 +44,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 */
-   /*  {
+     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'],isMobile:false },
-    },  */
+    },  
 
     /* Test against mobile viewports. */
      {
