58f0141683b92ec9dda53a542e33802ebefdcce6
Harry Fresco <harryfresco99@gmail.com>
2025-02-05 22:06:25 +0000

    added link from 404
    [skip netlify]

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-notfound',
-  imports: [CommonModule, FooterComponent],
+  imports: [CommonModule, FooterComponent, NavbarComponent],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.css'
 })
